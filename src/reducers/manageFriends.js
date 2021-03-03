4079793229c88c568652e3dc9e5cbf6786ddaa72
angela stephens <angelastephens317@gmail.com>
2021-03-02 21:20:24 -0500

    commit tree ad6d0a5e0ce7711d7a1d27b36e5d69845bd639ef

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends: []}, action){
+    if (action.type === "ADD_FRIEND"){
+        return {friends: [...state.friends,action.friend]}
+    }
+    else if (action.type === "REMOVE_FRIEND"){
+        let oldFriend = state.friends.filter(friend => friend.id !== action.id)
+        return {friends: oldFriend}
+    }
+    else {
+        return state
+    }
+
+
+
+    
 }
